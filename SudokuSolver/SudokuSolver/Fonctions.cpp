1cdff03b0f0c8141a04abc59df7962a235c588aa
Melissa <mel.beee@deepteal.com>
2014-12-01 11:59:27 -0500

    df
    sdf

--- SudokuSolver/SudokuSolver/Fonctions.cpp
+++ SudokuSolver/SudokuSolver/Fonctions.cpp
@@ -1,2 +1,40 @@
 #include "Fonctions.h"
 
+void AfficherMenu()
+{
+   cout << "  " << endl;
+}
+
+bool SwitchMenu()
+{
+
+}
+
+int FaireChoix()
+{
+
+}
+
+void Attendre()
+{
+   cout << endl << "Appuyer sur une touche pour continuer" << endl;
+   cin.ignore(cin.rdbuf()->in_avail() + 1);
+   system("cls");
+}
+////////////////////////////////////////////////////////////////////////////////
+void AfficherLigneSeparation()
+{
+   for (int i = 0; i < 50; i++)
+   {
+      cout << "="; // cout de paresseux :)
+   }
+   cout << endl;
+}
+////////////////////////////////////////////////////////////////////////////////
+void AfficherEntete(string entete)
+{
+   system("cls");
+   AfficherLigneSeparation();
+   cout << entete << endl;
+   AfficherLigneSeparation();
+}
--- SudokuSolver/SudokuSolver/Fonctions.h
+++ SudokuSolver/SudokuSolver/Fonctions.h
@@ -3,5 +3,11 @@
 
 #include "Sudoku.h"
 
+void AfficherMenu();
+bool SwitchMenu(); 
+int FaireChoix();
+void AfficherLigneSeparation();
+void AfficherEntete(string entete);
+void Attendre();
 
 #endif
