cafd1c2900abaf8af2f9ac9a45405daf70f69958
Brosseau <xavierb.2006@hotmail.com>
2014-12-02 12:58:32 -0500

    bitch

--- SudokuSolver/SudokuSolver/Sudoku.cpp
+++ SudokuSolver/SudokuSolver/Sudoku.cpp
@@ -12,34 +12,8 @@ Sudoku::Sudoku(string nom)
 {
 	monSudoku_.SetNbLignes(NBLIGNECOLONNE);
 	monSudoku_.SetNbColonnes(NBLIGNECOLONNE);
-
-	ifstream sudokuMap("Sudoku_"+nom+".txt");
-	char nombre;
-	if (!sudokuMap.fail()) //si le fichier est ouvert
-	{
-		for (int i = 0; i < NBLIGNECOLONNE; i++)
-		{
-			for (int j = 0; j < NBLIGNECOLONNE; j++)
-			{
-				sudokuMap.get(nombre);
-				if (nombre == '\n')		//vérifie si le caractère n'est pas un endl
-				{
-					sudokuMap.get(nombre);
-				}
-				if (nombre == '*') //Transforme les * en 0
-				{
-					nombre = '0';
-				}
-				cout << nombre;//*******************
-				monSudoku_.at(i).at(j) =  nombre-48;
-			}
-			cout << endl;
-		}
-	}
-	else
-	{
-		throw exception("le nom du fichier est introuvable");
-	}
+	ifstream sudokuMap("Sudoku_" + nom + ".txt");
+	RemplirMatrice(sudokuMap);
 }
 
 void Sudoku::Solutionner()
@@ -53,8 +27,43 @@ void Sudoku::Afficher(ostream & out)
 	{
 		for (int j = 0; j < NBLIGNECOLONNE; j++)
 		{
-			out << monSudoku_.at(i).at(j);
+			if (monSudoku_.at(i).at(j) != 0)
+			{
+				out << monSudoku_.at(i).at(j);
+			}
+			else
+			{
+				out << " ";
+			}
 		}
 		cout << endl;
+	}
+}
+
+void Sudoku::RemplirMatrice(ifstream & doc)
+{
+	char nombre;
+	if (!doc.fail()) //si le fichier est ouvert
+	{
+		for (int i = 0; i < NBLIGNECOLONNE; i++)
+		{
+			for (int j = 0; j < NBLIGNECOLONNE; j++)
+			{
+				doc.get(nombre);
+				if (nombre == '\n')//vérifie si le caractère n'est pas un endl
+				{
+					doc.get(nombre);
+				}
+				if (nombre == '*') //Transforme les * en int 0
+				{
+					nombre = '0';
+				}
+				monSudoku_.at(i).at(j) = nombre + CHARTOINTASCII;
+			}
+		}
+	}
+	else
+	{
+		throw exception("le nom du fichier est introuvable");
 	}
 }
--- SudokuSolver/SudokuSolver/Sudoku.h
+++ SudokuSolver/SudokuSolver/Sudoku.h
@@ -7,16 +7,20 @@
 #include <fstream>
 #include <sstream>
 using namespace std; 
-	const int NBLIGNECOLONNE = 9;
+	
 class Sudoku
 {
 public:
 	Sudoku(string nom);
 	void Solutionner();
 	void Afficher(ostream & out);
+	void RemplirMatrice(ifstream & doc);
 
 private:
-
+	//Ligne:9 Colonne:9
+	const int NBLIGNECOLONNE = 9;
+	//le code ascii -48 donne le int approprié
+	const int CHARTOINTASCII = -48;
 	Chrono horloge_;
 	Matrice<int> monSudoku_;
 };
